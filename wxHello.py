a3de8526ef8143392006c928cb7a75a5d83e298c
Barry F Smith <21973169+bfsmith9@users.noreply.github.com>
2019-02-26 21:59:44 -0500

    Added code for message in frame

--- wxHello.py
+++ wxHello.py
@@ -5,6 +5,12 @@ import wx
 class Frame(wx.Frame):
     def __init__(self, title):
         wx.Frame.__init__(self, None, title=title, size = (350,200))
+        pnl = wx.Panel(self)
+        st = wx.StaticText(pnl, label="What do you think?", pos=(40,50))
+        font = st.GetFont()
+        font.PointSize += 15
+        # font = font.Bold()
+        st.SetFont(font)
 # Create a new app; don't redirect stdout/stderr to a window
 app = wx.App(redirect=True)
 # A Frame is a top-level window
